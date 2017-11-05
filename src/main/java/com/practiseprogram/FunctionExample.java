e8e5007bb2d5bc94b31471fe60155fc46b50f214
Pratik <pratikaambani@gmail.com>
2017-11-05 16:56:45 +0530

    more examples

--- src/main/java/com/practiseprogram/FunctionExample.java
+++ src/main/java/com/practiseprogram/FunctionExample.java
@@ -5,7 +5,7 @@ import java.util.function.BiFunction;
 /**
  * Created by Pratik Ambani on 5/11/2017.
  */
-public class BiFunctionExample {
+public class FunctionExample {
     public static void main(String[] args) {
         BiFunction<Integer, Integer, Integer> biFunction = (a,b) -> a * b;
         System.out.println("biFunction: " +biFunction.apply(65,34));
